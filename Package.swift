46616761d0a088b6a0f6b7b57fdccc0a528f89be
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2026-05-03 10:33:15 +0200

    fix build

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/sparkle-project/Sparkle", exact: "2.9.1"),
+        .package(url: "https://github.com/jpsim/Yams", from: "5.1.0"),
     ],
     targets: [
         .target(
