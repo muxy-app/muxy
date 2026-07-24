baabcb6862ba511d77911fe77b146bfe132f9a74
Agustin Zamar <agustinzamar33@gmail.com>
2026-07-24 03:31:51 -0300

    fix: address tab-focused sidebar review feedback

--- Muxy/Models/Workspace/AppState.swift
+++ Muxy/Models/Workspace/AppState.swift
@@ -568,6 +568,12 @@ final class AppState {
         }
     }
 
+    func closeTabs(_ tabIDs: [UUID], areaID: UUID, key: WorktreeKey) {
+        for tabID in tabIDs {
+            closeTab(tabID, areaID: areaID, key: key)
+        }
+    }
+
     private func proceedCloseAfterVeto(_ tabID: UUID, areaID: UUID, key: WorktreeKey) {
         if needsProcessConfirmation(tabID: tabID, areaID: areaID, key: key) {
             pendingProcessTabClose = PendingTabClose(key: key, areaID: areaID, tabID: tabID)
